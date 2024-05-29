1fb4c88e94680a87d9a762c4d6db4a8070b5cbae
Kirill Fomichev <fanatid@ya.ru>
2024-05-28 23:19:35 -0400

    proto: add `timestamp` field to `SubscribeUpdate` message

--- yellowstone-grpc-geyser/src/grpc.rs
+++ yellowstone-grpc-geyser/src/grpc.rs
@@ -24,6 +24,7 @@ use {
             atomic::{AtomicUsize, Ordering},
             Arc,
         },
+        time::SystemTime,
     },
     tokio::{
         fs,
@@ -56,6 +57,7 @@ use {
             SubscribeUpdateSlot, SubscribeUpdateTransaction, SubscribeUpdateTransactionInfo,
             SubscribeUpdateTransactionStatus, TransactionError as SubscribeUpdateTransactionError,
         },
+        prost_types::Timestamp,
     },
 };
 
@@ -1315,6 +1317,7 @@ impl Geyser for GrpcService {
             let ping_msg = SubscribeUpdate {
                 filters: vec![],
                 update_oneof: Some(UpdateOneof::Ping(SubscribeUpdatePing {})),
+                timestamp: Some(Timestamp::from(SystemTime::now())),
             };
 
             loop {
